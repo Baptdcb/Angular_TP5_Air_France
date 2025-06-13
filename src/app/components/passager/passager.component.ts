2c671e99c51e122c74a0f082fe645605747f5121
BDC <baptiste.do-cabo@etu.univ-lyon1.fr>
2025-06-13 11:37:22 +0200

    feat: implement bagages directive for dynamic baggage styling based on passenger class

--- src/app/components/passager/passager.component.ts
+++ src/app/components/passager/passager.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Passager } from '../../models/passager.model';
 import { MatIcon } from '@angular/material/icon';
 import { ClasseVolDirective } from '../../directives/classe-vol.directive';
+import { BagagesDirective } from '../../directives/bagages.directive';
 
 @Component({
     selector: 'app-passager',
-    imports: [MatIcon, ClasseVolDirective],
+    imports: [MatIcon, ClasseVolDirective, BagagesDirective],
     templateUrl: './passager.component.html',
     styleUrls: ['./passager.component.scss']
 })
--- src/app/directives/bagages.directive.ts
+++ src/app/directives/bagages.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBagages]'
 })
-export class BagagesDirective {
+export class BagagesDirective implements OnChanges {
+  @Input('ClasseVol') appClasseVol!: string; 
+  @Input('appBagages') appBagages!: number;
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngOnChanges(): void {
+    this.updateRegles();
+  }
+
+  private updateRegles(): void {
+    let color = ''; 
+    if((this.appClasseVol === 'STANDARD' && this.appBagages > 1) ||
+       (this.appClasseVol === 'BUSINESS' && this.appBagages > 2) ||
+       (this.appClasseVol === 'PREMIUM' && this.appBagages > 3)) {
+      color = 'red'; 
+    } 
+    
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+
+  }
 }
